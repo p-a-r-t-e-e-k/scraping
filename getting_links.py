da4283fda89d09cf62c6a4fc817234b8e6ec2180
p-a-r-t-e-e-k <parteekkamboj9@gmail.com>
2023-01-12 22:43:32 +0530

    Additional information added

--- getting_links.py
+++ getting_links.py
@@ -1,8 +1,7 @@
-# import
+# imports
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from time import sleep
 import pandas as pd
 
@@ -18,7 +17,7 @@ sleep(3)
 
 # try to accept cooky
 try:
-    cooky=driver.find_element_by_xpath('//a[@id="cookieChoiceDismiss"]')
+    cooky=driver.find_element(By.XPATH,'//a[@id="cookieChoiceDismiss"]')
     cooky.click()
     sleep(1)
 except: pass
@@ -28,11 +27,13 @@ all_links=[]
 all_school_link=[]
 
 # getting the all ~20 links of region and adding in list (all_links)
-all_tag=driver.find_elements_by_xpath('//table[@class="ct"]//td/a')
+all_tag=driver.find_elements(By.XPATH,'//table[@class="ct"]//td/a')
 for one_tag in all_tag:
     href=one_tag.get_attribute("href")
     all_links.append(href)
-print(len(all_links),len(all_school_link))
+
+# length of outer and inner links before collection
+print(len(all_links),len(all_school_link),"------------------length before")
 
 # opening links in list (all_lisnks)
 # getting sublinks and adding in list (all_school_link)
@@ -40,17 +41,19 @@ for link in all_links:
     driver.get(link)
     sleep(1)
     try:
-        if driver.find_element_by_xpath('//table[@class="ct"]//td/a'):
-            all_tag=driver.find_elements_by_xpath('//table[@class="ct"]//td/a')
+        if driver.find_element(By.XPATH,'//table[@class="ct"]//td/a'):
+            all_tag=driver.find_elements(By.XPATH,'//table[@class="ct"]//td/a')
             for one_tag in all_tag:
                 href=one_tag.get_attribute("href")
                 all_school_link.append(href)
-            print(len(all_school_link),".......")
+            print(len(all_school_link),"----------------no. of href")
 
     except Exception as e:
         print(e,"---------------------in except")
 
-        
+# length of outer and inner links after collection
+print(len(all_links),len(all_school_link),"------------------length-after")
+
 # creating dataframe of list (all_school_link)
 data1={
     "Page Link":all_school_link
@@ -62,5 +65,5 @@ sleep(0.5)
 # making csv
 df1.to_csv('tuttitalia_links.csv',index=False)
 
-# quit driver and close/stop all
+# quit driver and close/stop all process
 driver.quit()
--- information.py
+++ information.py
@@ -1,15 +1,17 @@
+# imports
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 import pandas as pd
 
+# creating driver with chrome, maximize window, clear cooky
 driver=webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 driver.delete_all_cookies()
 
-
+# declaring dictionary for first dataframe-> df1
 data1={
     "Region":[''],
     "Provincia":"",
@@ -20,43 +22,55 @@ data1={
     "Type":"",
     "Code":""
 }
-
 df1=pd.DataFrame(data1)
 
-# all_links=[]df=pd.read_csv('tuttitalia_School_links.csv')
-# all_links=df['School Link']
-# print(len(all_links),"*****")
-all_links=['https://www.tuttitalia.it/abruzzo/58-altino/89-scuole/','https://www.tuttitalia.it/abruzzo/61-frisa/76-scuole/','https://www.tuttitalia.it/abruzzo/41-rapino/66-scuole/']
+# getting links from tuttitalia_links.csv from getting_links.py
+df = pd.read_csv('getting_links.py')
+all_links=df['Page Link']
+print(len(all_links),"-------------total links in csv/list")
+
+# itrate links
+count=1
 for url in all_links:
+    print(url,"---------------------url-> "+str(count)+"/"+str(len(all_links)))
+    count+=1
     driver.get(url)
     sleep(2)
+
+    # try to accept cooky
     try:
-        cooky=driver.find_element_by_xpath('//a[@id="cookieChoiceDismiss"]')
+        cooky=driver.find_element(By.XPATH,'//a[@id="cookieChoiceDismiss"]')
         cooky.click()
         sleep(1)
     except Exception as e: pass
+
+    # getting region, provincia, city,
     region=[]
-    rgn=driver.find_element_by_xpath('//div[@id="ic"]/a[3]').text
+    rgn=driver.find_element(By.XPATH,'//div[@id="ic"]/a[3]').text
     region.append(rgn)
-    provincia=driver.find_element_by_xpath('//div[@id="ic"]/a[4]').text
-    city=driver.find_element_by_xpath('//div[@id="ic"]/a[5]').text
-    outer_box=driver.find_elements_by_xpath('//div[@class="aj"]')
+    provincia=driver.find_element(By.XPATH,'//div[@id="ic"]/a[4]').text
+    city=driver.find_element(By.XPATH,'//div[@id="ic"]/a[5]').text
+
+    # getting grade, school name, address, type, school code
+    outer_box=driver.find_elements(By.XPATH,'//div[@class="aj"]')
     for one_outer in outer_box:
-        grade=one_outer.find_element_by_xpath('./div[1]/h2').text
+        grade=one_outer.find_element(By.XPATH,'./div[1]/h2').text
         try:
-            box_in_otr=one_outer.find_elements_by_xpath('./div[@class="if"]')
+            box_in_otr=one_outer.find_elements(By.XPATH,'./div[@class="if"]')
             for one_box in box_in_otr:
                 action=ActionChains(driver)
                 action.move_to_element(one_box)
                 sleep(0.5)
                 try:
-                    sch_name=one_box.find_element_by_xpath('./div[2]/h3').text
-                    addr=one_box.find_element_by_xpath('./div[2]').text
-                    type=one_box.find_element_by_xpath('./div[3]/i').text
-                    code=one_box.find_element_by_xpath('./div[3]/code').text
+                    sch_name=one_box.find_element(By.XPATH,'./div[2]/h3').text
+                    addr=one_box.find_element(By.XPATH,'./div[2]').text
+                    type=one_box.find_element(By.XPATH,'./div[3]/i').text
+                    code=one_box.find_element(By.XPATH,'./div[3]/code').text
                     sleep(0.5)
                 except Exception as e:
                     print(e)
+
+                # making 2nd dataframe(df2) and append in first dataframe(df1)
                 try:
                     data2={
                             "Region":region,
@@ -72,8 +86,12 @@ for url in all_links:
                     df1=df1.append(df2, ignore_index = True)
                 except Exception as e:
                         print(e)
+
         except Exception as e:
             print(e)
+
+# making final csv
 df1.to_csv("Inform.csv")
 
-
+# quit driver and close/stop all process
+driver.quit()
